7a33d06cc5ca4df5550b4a2911b196f5ab71fcca
Joao Victor Degelo <jvtdegelo@gmail.com>
2022-05-10 21:55:57 -0300

    added a docker compose for aula 5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import express from "express";
 import productRouter from "./routes/products.js"
+import mongoose from "mongoose";
+
 const PORT = 3000;
 
-const server = express();
-server.use(express.json());
-server.use("/produtos", productRouter)
+// const server = express();
+// server.use(express.json());
+// server.use("/produtos", productRouter)
 
 
-server.listen(PORT, ()=>{
-    console.log(`listening on ${PORT}`);
-});
+// server.listen(PORT, ()=>{
+//     console.log(`listening on ${PORT}`);
+// });
+
+mongoose.connect("mongodb://localhost:27017/aula_db")
+    .then(() => console.log("Conectado"));
+
+
+const productModel = mongoose.model("Product");
+
+const meuProduto = new productModel();
+meuProduto.name = "shampoo";
+meuProducto.save()
+    .then(()=>)
