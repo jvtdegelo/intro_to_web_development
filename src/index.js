f3b9fc19e02721dd59c3f157f0b79179daaa8760
Joao Victor Degelo <jvtdegelo@gmail.com>
2022-05-12 21:48:37 -0300

    Very messy implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// import express from "express";
-// import productRouter from "./routes/products.js"
+import express from "express";
+import productRouter from "./routes/products.js"
 import mongoose from "mongoose";
 
 const PORT = 3000;
 
-// const server = express();
-// server.use(express.json());
-// server.use("/produtos", productRouter)
+const server = express();
+server.use(express.json());
+//server.use("/produtos", productRouter)
 
 
-// server.listen(PORT, ()=>{
-//     console.log(`listening on ${PORT}`);
-// });
+server.listen(PORT, ()=>{
+    console.log(`listening on ${PORT}`);
+});
 
 mongoose.connect("mongodb://localhost:27017/aula_db")
     .then(() => console.log("Conectado"));
@@ -24,13 +24,25 @@ const ProductSchema = new mongoose.Schema({
 const productModel = mongoose.model("Product", ProductSchema);
 
 const meuProduto = new productModel();
-meuProduto.name = "shampoo";
-meuProduto.save()
-  .then(() => console.log("Salvo"))
-  .catch((e) => console.error(e));
+// meuProduto.name = "shampoo";
+// meuProduto.save()
+//   .then(() => console.log("Salvo"))
+//   .catch((e) => console.error(e));
 
 
-productModel
-    .find()
-    .then((objs) => console.log(objs))
-    .catch((error) => console.error(error))
+// productModel
+//     .find()
+//     .then((objs) => console.log(objs))
+//     .catch((error) => console.error(error));
+    server.get('/produtos/:name', (request, response) => {
+        productModel
+        .find({"name":request.params.name})
+        .then((objs) => console.log(objs))
+        .catch((error) => console.error(error));
+    
+        response
+          .status(201)
+          .json(request.body);
+      });
+
+
