a33fbc3547d200cf317ce29b4e3834cc51eb4a90
Joao Victor Degelo <jvtdegelo@gmail.com>
2022-05-23 21:35:38 -0300

    adiciona algumas melhorias

--- aula-react/src/components/FloatingCard.js
+++ aula-react/src/components/FloatingCard.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const FloatingCard = () => {
   return (
-    <div style={{width:'100%', height: '50%', border-radius: '25pt', border:'1px solid black'}}>
+    <div style={{width:'100%', height: '50%',  border:'1px solid black'}}>
       <div style = {{margin:'5pt'}}>
         <h1>Titulo</h1>
       </div>
--- aula-react/src/components/TopNavBar.js
+++ aula-react/src/components/TopNavBar.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 function TopNavBar(){
+  const [barraPesquisa, setBarra] = useState ("amem");
+  const [input, setInput] = useState("");
+
+  const handleClick = () =>{
+    console.log("clicado")
+    setBarra(input);
+  }
   return(
     <div>
-      <div>
-        pesquisa
+      <div style={{display:'flex'}}>
+        <div>
+          <input value={input} onInput={e => setInput(e.target.value)}/>
+        </div>
+        <button onClick={handleClick}>pesquisar</button>
+        <span>{barraPesquisa}</span>
       </div>
       <div style = {{display:'flex', margin :'5pt'}}>
         <div style = {{margin :'5pt', color:'darkgray'}}>Todas</div>
